8ec22ba09cac6234cc392de49e3001832a4f36de
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2017-02-20 17:21:24 +0100

    changed karma server port to be able to test while serving the app

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
     exclude: [],
 
     // web server port
-    port: 9000,
+    port: 9050,
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
