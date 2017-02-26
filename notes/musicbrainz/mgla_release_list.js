4d376e57b19aca2ee95d0e48387c10162b19de4f
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2017-02-26 15:57:38 +0100

    added notes for musicbrainz api

---
+++ notes/musicbrainz/mgla_release_list.js
@@ -0,0 +1,594 @@
+{
+  "name": "Mgła",
+  "id": "e7b4c928-8c45-4ecf-9f08-da64fe5c2ddd",
+  "disambiguation": "",
+  "isnis": [],
+  "end_area": null,
+  "type": "Group",
+  "country": "PL",
+  "begin_area": {
+    "sort-name": "Kraków",
+    "disambiguation": "",
+    "id": "e82975d0-7258-4e4c-9c56-6e1deea8ad4c",
+    "name": "Kraków"
+  },
+  "area": {
+    "sort-name": "Poland",
+    "disambiguation": "",
+    "id": "dd7f80c8-f017-3d01-8608-2a8c9c32b954",
+    "name": "Poland",
+    "iso-3166-1-codes": [
+      "PL"
+    ]
+  },
+  "sort-name": "Mgla",
+  "type-id": "e431f5f6-b5d2-343d-8b36-72607fffb74b",
+  "gender": null,
+  "ipis": [],
+  "life-span": {
+    "ended": false,
+    "end": null,
+    "begin": "2000"
+  },
+  "gender-id": null,
+  "releases": [
+    {
+      "country": "PL",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "title": "With Hearts Toward None",
+      "packaging-id": null,
+      "status": "Official",
+      "barcode": null,
+      "date": "2012-02-28",
+      "quality": "normal",
+      "release-events": [
+        {
+          "date": "2012-02-28",
+          "area": {
+            "iso-3166-1-codes": [
+              "PL"
+            ],
+            "disambiguation": "",
+            "id": "dd7f80c8-f017-3d01-8608-2a8c9c32b954",
+            "sort-name": "Poland",
+            "name": "Poland"
+          }
+        }
+      ],
+      "packaging": null,
+      "text-representation": {
+        "language": "eng",
+        "script": "Latn"
+      },
+      "id": "007b1cff-c27b-4924-92e3-fd59a03beeb5",
+      "disambiguation": ""
+    },
+    {
+      "id": "00823246-94c3-4d2f-a865-54c0f8d343a0",
+      "disambiguation": "",
+      "release-events": [
+        {
+          "date": "2015-09-04",
+          "area": {
+            "iso-3166-1-codes": [
+              "XW"
+            ],
+            "name": "[Worldwide]",
+            "sort-name": "[Worldwide]",
+            "disambiguation": "",
+            "id": "525d4e18-3d00-31b9-a58b-a146a916de8f"
+          }
+        }
+      ],
+      "packaging": "None",
+      "text-representation": {
+        "language": "eng",
+        "script": "Latn"
+      },
+      "date": "2015-09-04",
+      "quality": "normal",
+      "status": "Official",
+      "barcode": "",
+      "title": "Exercises in Futility",
+      "packaging-id": "119eba76-b343-3e02-a292-f0f00644bb9b",
+      "country": "XW",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe"
+    },
+    {
+      "status": "Official",
+      "barcode": "",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "country": "FI",
+      "title": "Crushing the Holy Trinity",
+      "packaging-id": "f7101ce3-0384-39ce-9fde-fbbd0044d35f",
+      "release-events": [
+        {
+          "date": "2005-07-12",
+          "area": {
+            "iso-3166-1-codes": [
+              "FI"
+            ],
+            "sort-name": "Finland",
+            "disambiguation": "",
+            "id": "6a264f94-6ff1-30b1-9a81-41f7bfabd616",
+            "name": "Finland"
+          }
+        }
+      ],
+      "packaging": "Cardboard/Paper Sleeve",
+      "text-representation": {
+        "script": "Latn",
+        "language": "eng"
+      },
+      "id": "0f6e2d16-8610-46c5-b7e3-8bf0d472217b",
+      "disambiguation": "",
+      "date": "2005-07-12",
+      "quality": "normal"
+    },
+    {
+      "date": "2016",
+      "quality": "normal",
+      "id": "124f1f9b-efe5-4db8-8309-c321452bf0fe",
+      "disambiguation": "",
+      "release-events": [
+        {
+          "date": "2016",
+          "area": {
+            "iso-3166-1-codes": [
+              "PL"
+            ],
+            "sort-name": "Poland",
+            "disambiguation": "",
+            "id": "dd7f80c8-f017-3d01-8608-2a8c9c32b954",
+            "name": "Poland"
+          }
+        }
+      ],
+      "packaging": "Cardboard/Paper Sleeve",
+      "text-representation": {
+        "script": "Latn",
+        "language": "mul"
+      },
+      "title": "Mdłości / Further Down the Nest",
+      "packaging-id": "f7101ce3-0384-39ce-9fde-fbbd0044d35f",
+      "country": "PL",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "status": "Official",
+      "barcode": ""
+    },
+    {
+      "release-events": [
+        {
+          "date": "2007-06-19",
+          "area": {
+            "id": "dd7f80c8-f017-3d01-8608-2a8c9c32b954",
+            "disambiguation": "",
+            "sort-name": "Poland",
+            "name": "Poland",
+            "iso-3166-1-codes": [
+              "PL"
+            ]
+          }
+        }
+      ],
+      "text-representation": {
+        "language": "eng",
+        "script": "Latn"
+      },
+      "packaging": null,
+      "disambiguation": "",
+      "id": "404711fd-a93e-441e-a5e7-09a43c88a93a",
+      "date": "2007-06-19",
+      "quality": "normal",
+      "status": "Official",
+      "barcode": "",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "country": "PL",
+      "title": "Further Down the Nest",
+      "packaging-id": null
+    },
+    {
+      "packaging": "Jewel Case",
+      "text-representation": {
+        "language": "eng",
+        "script": "Latn"
+      },
+      "release-events": [
+        {
+          "date": "2012-02-28",
+          "area": {
+            "disambiguation": "",
+            "id": "6a264f94-6ff1-30b1-9a81-41f7bfabd616",
+            "sort-name": "Finland",
+            "name": "Finland",
+            "iso-3166-1-codes": [
+              "FI"
+            ]
+          }
+        }
+      ],
+      "disambiguation": "",
+      "id": "557526dd-b67f-4c5c-ab26-a8049ec4974c",
+      "quality": "normal",
+      "date": "2012-02-28",
+      "barcode": "",
+      "status": "Official",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "country": "FI",
+      "packaging-id": "ec27701a-4a22-37f4-bfac-6616e0f9750a",
+      "title": "With Hearts Toward None"
+    },
+    {
+      "country": "FI",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "packaging-id": "ec27701a-4a22-37f4-bfac-6616e0f9750a",
+      "title": "Presence",
+      "barcode": "",
+      "status": "Official",
+      "date": "2006-01-30",
+      "quality": "normal",
+      "text-representation": {
+        "script": "Latn",
+        "language": "eng"
+      },
+      "packaging": "Jewel Case",
+      "release-events": [
+        {
+          "area": {
+            "name": "Finland",
+            "disambiguation": "",
+            "id": "6a264f94-6ff1-30b1-9a81-41f7bfabd616",
+            "sort-name": "Finland",
+            "iso-3166-1-codes": [
+              "FI"
+            ]
+          },
+          "date": "2006-01-30"
+        }
+      ],
+      "disambiguation": "",
+      "id": "6960fd48-7fce-41d4-898d-bd13b53fc30e"
+    },
+    {
+      "status": "Official",
+      "barcode": "",
+      "title": "With Hearts Toward None",
+      "packaging-id": "119eba76-b343-3e02-a292-f0f00644bb9b",
+      "country": "XW",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "disambiguation": "",
+      "id": "6b73cb03-be75-4b0a-ae85-703af106aafb",
+      "release-events": [
+        {
+          "area": {
+            "iso-3166-1-codes": [
+              "XW"
+            ],
+            "sort-name": "[Worldwide]",
+            "disambiguation": "",
+            "id": "525d4e18-3d00-31b9-a58b-a146a916de8f",
+            "name": "[Worldwide]"
+          },
+          "date": "2015-08-24"
+        }
+      ],
+      "text-representation": {
+        "script": "Latn",
+        "language": "eng"
+      },
+      "packaging": "None",
+      "date": "2015-08-24",
+      "quality": "normal"
+    },
+    {
+      "release-events": [
+        {
+          "date": "2015-09-04",
+          "area": {
+            "name": "Finland",
+            "sort-name": "Finland",
+            "disambiguation": "",
+            "id": "6a264f94-6ff1-30b1-9a81-41f7bfabd616",
+            "iso-3166-1-codes": [
+              "FI"
+            ]
+          }
+        }
+      ],
+      "packaging": "Jewel Case",
+      "text-representation": {
+        "script": "Latn",
+        "language": "eng"
+      },
+      "disambiguation": "",
+      "id": "6c766408-3ade-4e9c-bb2a-ecade846a42e",
+      "date": "2015-09-04",
+      "quality": "normal",
+      "status": "Official",
+      "barcode": "",
+      "country": "FI",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "title": "Exercises in Futility",
+      "packaging-id": "ec27701a-4a22-37f4-bfac-6616e0f9750a"
+    },
+    {
+      "packaging-id": "f7101ce3-0384-39ce-9fde-fbbd0044d35f",
+      "title": "With Hearts Toward None",
+      "country": "FI",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "barcode": "",
+      "status": "Official",
+      "quality": "normal",
+      "date": "2012-03-16",
+      "disambiguation": "",
+      "id": "7c75fdb9-5517-4dad-922a-a0c1175bd3f7",
+      "text-representation": {
+        "language": "eng",
+        "script": "Latn"
+      },
+      "packaging": "Cardboard/Paper Sleeve",
+      "release-events": [
+        {
+          "area": {
+            "id": "6a264f94-6ff1-30b1-9a81-41f7bfabd616",
+            "disambiguation": "",
+            "sort-name": "Finland",
+            "name": "Finland",
+            "iso-3166-1-codes": [
+              "FI"
+            ]
+          },
+          "date": "2012-03-16"
+        }
+      ]
+    },
+    {
+      "title": "Mdłości + Further Down the Nest",
+      "packaging-id": "ec27701a-4a22-37f4-bfac-6616e0f9750a",
+      "country": "PL",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "status": "Official",
+      "barcode": "",
+      "date": "2012",
+      "quality": "normal",
+      "disambiguation": "",
+      "id": "82698ecd-adea-4a78-95aa-17a6a26222e7",
+      "release-events": [
+        {
+          "date": "2012",
+          "area": {
+            "sort-name": "Poland",
+            "disambiguation": "",
+            "id": "dd7f80c8-f017-3d01-8608-2a8c9c32b954",
+            "name": "Poland",
+            "iso-3166-1-codes": [
+              "PL"
+            ]
+          }
+        }
+      ],
+      "packaging": "Jewel Case",
+      "text-representation": {
+        "script": "Latn",
+        "language": "eng"
+      }
+    },
+    {
+      "status": "Official",
+      "barcode": null,
+      "title": "Mdłości / Further Down the Nest",
+      "packaging-id": null,
+      "country": "PL",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "disambiguation": "",
+      "id": "873f4d36-bafc-4850-85b4-d74962012db2",
+      "release-events": [
+        {
+          "area": {
+            "name": "Poland",
+            "sort-name": "Poland",
+            "disambiguation": "",
+            "id": "dd7f80c8-f017-3d01-8608-2a8c9c32b954",
+            "iso-3166-1-codes": [
+              "PL"
+            ]
+          },
+          "date": "2007-05-30"
+        }
+      ],
+      "packaging": null,
+      "text-representation": {
+        "script": "Latn",
+        "language": "eng"
+      },
+      "date": "2007-05-30",
+      "quality": "normal"
+    },
+    {
+      "country": "PL",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "packaging-id": null,
+      "title": "Mdłości",
+      "barcode": "",
+      "status": "Official",
+      "date": "2006-09-03",
+      "quality": "normal",
+      "packaging": null,
+      "text-representation": {
+        "language": "pol",
+        "script": "Latn"
+      },
+      "release-events": [
+        {
+          "date": "2006-09-03",
+          "area": {
+            "iso-3166-1-codes": [
+              "PL"
+            ],
+            "name": "Poland",
+            "id": "dd7f80c8-f017-3d01-8608-2a8c9c32b954",
+            "disambiguation": "",
+            "sort-name": "Poland"
+          }
+        }
+      ],
+      "disambiguation": "",
+      "id": "8bd219b2-5918-4e46-8d66-9c9067206597"
+    },
+    {
+      "id": "a4cf77da-b9b0-46e4-a705-cd6664088d6a",
+      "disambiguation": "first press",
+      "packaging": "Cardboard/Paper Sleeve",
+      "text-representation": {
+        "script": "Latn",
+        "language": "eng"
+      },
+      "release-events": [
+        {
+          "date": "2016-01-06",
+          "area": {
+            "iso-3166-1-codes": [
+              "FI"
+            ],
+            "sort-name": "Finland",
+            "id": "6a264f94-6ff1-30b1-9a81-41f7bfabd616",
+            "disambiguation": "",
+            "name": "Finland"
+          }
+        }
+      ],
+      "quality": "normal",
+      "date": "2016-01-06",
+      "barcode": "",
+      "status": "Official",
+      "packaging-id": "f7101ce3-0384-39ce-9fde-fbbd0044d35f",
+      "title": "Exercises in Futility",
+      "country": "FI",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe"
+    },
+    {
+      "date": "2013-05-02",
+      "quality": "normal",
+      "text-representation": {
+        "script": "Latn",
+        "language": "eng"
+      },
+      "packaging": "Cardboard/Paper Sleeve",
+      "release-events": [
+        {
+          "date": "2013-05-02",
+          "area": {
+            "id": "6a264f94-6ff1-30b1-9a81-41f7bfabd616",
+            "disambiguation": "",
+            "sort-name": "Finland",
+            "name": "Finland",
+            "iso-3166-1-codes": [
+              "FI"
+            ]
+          }
+        }
+      ],
+      "disambiguation": "",
+      "id": "a8b11703-fab6-41fe-9559-500bd8aebff5",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "country": "FI",
+      "packaging-id": "f7101ce3-0384-39ce-9fde-fbbd0044d35f",
+      "title": "Presence / Power and Will",
+      "barcode": "",
+      "status": "Official"
+    },
+    {
+      "disambiguation": "",
+      "id": "b7ad9ff3-6df3-4298-b8f6-d8e34354d49e",
+      "text-representation": {
+        "language": "mul",
+        "script": "Latn"
+      },
+      "packaging": null,
+      "release-events": [
+        {
+          "date": "2010",
+          "area": {
+            "disambiguation": "",
+            "id": "dd7f80c8-f017-3d01-8608-2a8c9c32b954",
+            "sort-name": "Poland",
+            "name": "Poland",
+            "iso-3166-1-codes": [
+              "PL"
+            ]
+          }
+        }
+      ],
+      "date": "2010",
+      "quality": "normal",
+      "barcode": null,
+      "status": "Official",
+      "packaging-id": null,
+      "title": "Mdłości / Further Down the Nest",
+      "country": "PL",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe"
+    },
+    {
+      "date": "2005-07-11",
+      "quality": "normal",
+      "text-representation": {
+        "script": "Latn",
+        "language": "eng"
+      },
+      "packaging": "Cardboard/Paper Sleeve",
+      "release-events": [
+        {
+          "area": {
+            "iso-3166-1-codes": [
+              "FI"
+            ],
+            "sort-name": "Finland",
+            "disambiguation": "",
+            "id": "6a264f94-6ff1-30b1-9a81-41f7bfabd616",
+            "name": "Finland"
+          },
+          "date": "2005-07-11"
+        }
+      ],
+      "disambiguation": "",
+      "id": "e6114a54-81e2-4140-b304-6e92b317e51c",
+      "country": "FI",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe",
+      "packaging-id": "f7101ce3-0384-39ce-9fde-fbbd0044d35f",
+      "title": "Crushing the Holy Trinity",
+      "barcode": "",
+      "status": "Official"
+    },
+    {
+      "id": "fdce9973-3b91-4ccb-8650-6012e8bd2065",
+      "disambiguation": "",
+      "release-events": [
+        {
+          "area": {
+            "id": "6a264f94-6ff1-30b1-9a81-41f7bfabd616",
+            "disambiguation": "",
+            "sort-name": "Finland",
+            "name": "Finland",
+            "iso-3166-1-codes": [
+              "FI"
+            ]
+          },
+          "date": "2008-08-13"
+        }
+      ],
+      "text-representation": {
+        "language": "pol",
+        "script": "Latn"
+      },
+      "packaging": "Jewel Case",
+      "date": "2008-08-13",
+      "quality": "normal",
+      "status": "Official",
+      "barcode": "",
+      "title": "Groza",
+      "packaging-id": "ec27701a-4a22-37f4-bfac-6616e0f9750a",
+      "country": "FI",
+      "status-id": "4e304316-386d-3409-af2e-78857eec5cfe"
+    }
+  ]
+}
